8ee072742e337603a1834a84d36d4e06f766ddcf
Anton Osmolovskyi <tony.osmolovsky@gmail.com>
2022-05-04 15:11:22 +0200

    Add Eurasia trip class

---
+++ Euroasia.java
@@ -0,0 +1,10 @@
+package classwork10;
+
+public class Euroasia {
+	String[] countries = {"Poland", "Ukraine", "Germany", "Russia"};
+	public void printCountries() {
+		for (int i = 0; i < countries.length; i++) {
+			System.out.println(countries[i]);
+		}
+	}
+}
